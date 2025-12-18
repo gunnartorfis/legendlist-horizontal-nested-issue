172ee888f3ac82f2e4ee44f56f67e3b143604408
Gunnar Torfi <gunnartorfis@gmail.com>
2025-12-18 00:18:44 +0000

    chore: text

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import Broken from "../components/broken";
 import Working from "../components/working";
 
 export default function App() {
   return (
     <View style={{ flex: 1, marginTop: 100 }}>
+      <Text>Broken</Text>
       <Broken />
       <View
         style={{ height: 1, backgroundColor: "blue", marginVertical: 20 }}
       />
+      <Text>Working with the workaround</Text>
       <Working />
     </View>
   );
